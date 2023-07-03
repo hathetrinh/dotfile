49ee83e5af68bb22c7a648ffd86885d4fd038c58
Trinh Ha <trinh.the.ha@mgm-tp.com>
2023-07-03 13:17:38 +0700

    fix update

--- nvim/lua/trinhha/plugin.lua
+++ nvim/lua/trinhha/plugin.lua
@@ -170,30 +170,6 @@ return require("packer").startup(function(use)
 
     use("sindrets/diffview.nvim")
 
-    use {
-  "mfussenegger/nvim-dap",
-  opt = true,
-  module = { "dap" },
-  requires = {
-    "theHamsta/nvim-dap-virtual-text",
-    "rcarriga/nvim-dap-ui",
-    "mfussenegger/nvim-dap-python",
-    "nvim-telescope/telescope-dap.nvim",
-    { "leoluz/nvim-dap-go", module = "dap-go" },
-    { "jbyuki/one-small-step-for-vimkind", module = "osv" },
-    { "mxsdev/nvim-dap-vscode-js", module = { "dap-vscode-js" } }
-    {
-      "microsoft/vscode-js-debug",
-      opt = true,
-      run = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out" 
-    },
-  },
-  config = function()
-    require("config.dap").setup()
-  end,
-  disable = false,
-}
-
     if packer_bootstrap then
         require("packer").sync()
     end
