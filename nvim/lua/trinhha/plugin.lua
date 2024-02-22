9cf430409d42006e126d9499b8de89f3f4e7ebdc
Trinh Ha <trinh.the.ha@mgm-tp.com>
2024-02-22 11:35:15 +0700

    update plugin

--- nvim/lua/trinhha/plugin.lua
+++ nvim/lua/trinhha/plugin.lua
@@ -138,6 +138,7 @@ return require("packer").startup(function(use)
     })
 
     use("tpope/vim-surround")
+    use("windwp/nvim-ts-autotag");
 
     use({
         "lewis6991/gitsigns.nvim",
@@ -212,6 +213,8 @@ return require("packer").startup(function(use)
     use("nvim-telescope/telescope-dap.nvim")
 
     use("ThePrimeagen/git-worktree.nvim")
+
+    --use("mfussenegger/nvim-jdtls")
 
     if packer_bootstrap then
         require("packer").sync()
