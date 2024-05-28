ac18a479b8d480633fcd7df4ed16142c7e91e844
Trinh Ha <trinh.the.ha@mgm-tp.com>
2024-05-28 13:35:05 +0700

    update telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -13,10 +13,21 @@ require("telescope").setup({
 			theme = "dropdown",
 		},
 	},
+	extensions = {
+		fzf = {
+			fuzzy = false, -- false will only do exact matching
+			override_generic_sorter = true, -- override the generic sorter
+			override_file_sorter = true, -- override the file sorter
+			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+			-- case_mode = "ignore_case" -- or "ignore_case" or "respect_case"
+		},
+	},
 })
 require("telescope").load_extension("luasnip")
 require("telescope").load_extension("dap")
 require("telescope").load_extension("git_worktree")
+require("telescope").load_extension("fzf")
+
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<cr>", {})
 vim.keymap.set("n", "<leader>ft", ":Telescope live_grep<cr>", {})
